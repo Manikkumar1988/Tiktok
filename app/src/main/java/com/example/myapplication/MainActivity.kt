d9e3544fc376c1d002d317c52b74317d15e497d0
Manikkumar1988 <maniksrr@gmail.com>
2019-08-16 23:15:32 +0530

    navigation added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -6,19 +6,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import com.luseen.spacenavigation.SpaceItem
 import com.luseen.spacenavigation.SpaceNavigationView
+import android.widget.Toast
+import com.luseen.spacenavigation.SpaceOnClickListener
+
+
 
 
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var spaceNavigationView: SpaceNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val spaceNavigationView = findViewById(R.id.space) as SpaceNavigationView
+       /* spaceNavigationView = findViewById(R.id.space) as SpaceNavigationView
         spaceNavigationView.initWithSaveInstanceState(savedInstanceState)
         spaceNavigationView.addSpaceItem(SpaceItem("HOME", R.drawable.ic_home_black_24dp))
-        spaceNavigationView.addSpaceItem(SpaceItem("SEARCH", R.drawable.ic_home_black_24dp))
+        spaceNavigationView.changeCenterButtonIcon(R.drawable.ic_launcher_foreground)
+        spaceNavigationView.addSpaceItem(SpaceItem("PROFILE", R.drawable.ic_home_black_24dp))
+
+
+        spaceNavigationView.setSpaceOnClickListener(object : SpaceOnClickListener {
+            override fun onCentreButtonClick() {
+                Toast.makeText(this@MainActivity, "onCentreButtonClick", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onItemClick(itemIndex: Int, itemName: String) {
+                Toast.makeText(this@MainActivity, "$itemIndex $itemName", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onItemReselected(itemIndex: Int, itemName: String) {
+                Toast.makeText(this@MainActivity, "$itemIndex $itemName", Toast.LENGTH_SHORT).show()
+            }
+        })*/
     }
 }
